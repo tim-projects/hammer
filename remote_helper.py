618ca5f218249f31d6156922c724ea6fc33bd8ab
Test User <test@example.com>
2026-04-29 00:03:54 +0100

    Cleanup: remove temporary helper file

--- remote_helper.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def get_remote():
-    res = subprocess.run(["git", "remote"], capture_output=True, text=True)
-    remotes = res.stdout.split()
-    if "origin" in remotes:
-        return "origin"
-    return remotes[0] if remotes else "origin"
